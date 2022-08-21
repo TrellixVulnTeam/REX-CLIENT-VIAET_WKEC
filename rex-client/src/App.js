dd1b9d4fdac26b560e63548acc13e750432f65d4
Er. Vineesh Cutting <vineesh2pro@yahoo.co.in>
2022-08-21 09:44:49 +0000

    Adding BattleField Code

--- rex-client/src/App.js
+++ rex-client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Battlefield from "./Components/Battlefield"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Battlefield />}>
+              <Route index element={<Battlefield />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
