adf01f5f1e33921e6538cf28a025776bdb0e5dc2
Er. Vineesh Cutting <vineesh2pro@yahoo.co.in>
2022-08-25 06:48:43 +0000

    Create Home for Rex

--- my-rex-app/src/App.js
+++ my-rex-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import  Home  from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <h1>Welcome to Rex Client!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
