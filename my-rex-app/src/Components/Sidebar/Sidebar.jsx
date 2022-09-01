2424bbd4e37680bd09bcf0432584c32de39dc748
Er. Vineesh Cutting <vineesh2pro@yahoo.co.in>
2022-09-01 21:51:52 +0530

    Shuats Logo Added

--- my-rex-app/src/Components/Sidebar/Sidebar.jsx
+++ my-rex-app/src/Components/Sidebar/Sidebar.jsx
@@ -5,8 +5,8 @@ function Sidebar() {
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
             {/* Brand Logo */}
             <a href="index3.html" className="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-                <span className="brand-text font-weight-light">AdminLTE 3</span>
+                <img src="dist/img/ShuatsLogo.png" alt="Shuats Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
+                <span className="brand-text font-weight-light">REX VIAET</span>
             </a>
             {/* Sidebar */}
             <div className="sidebar">
@@ -16,7 +16,7 @@ function Sidebar() {
                         <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
                     </div>
                     <div className="info">
-                        <a href="#" className="d-block">Alexander Pierce</a>
+                        <a href="#" className="d-block">Arpan Sherring</a>
                     </div>
                 </div>
                 {/* SidebarSearch Form */}
